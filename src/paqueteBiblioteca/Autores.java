71e337ad59b551b2195e398db22e9451ddd3c455
Miikeel16 <mikel.garcia06@somo.eus>
2025-01-28 22:03:20 +0100

    Guardar getters y setters

--- src/paqueteBiblioteca/Autores.java
+++ src/paqueteBiblioteca/Autores.java
@@ -1,10 +1,39 @@
 package paqueteBiblioteca;
 
-public class Autores {
+		class Autor {
+		    private int id;
+		    private String nombre;
+		    private String apellidos;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		    public Autor(int id, String nombre, String apellidos) {
+		        this.id = id;
+		        this.nombre = nombre;
+		        this.apellidos = apellidos;
+		    }
 
+		    public int getId() {
+		        return id;
+		    }
+
+		    public void setId(int id) {
+		        this.id = id;
+		    }
+
+		    public String getNombre() {
+		        return nombre;
+		    }
+
+		    public void setNombre(String nombre) {
+		        this.nombre = nombre;
+		    }
+
+		    public String getApellidos() {
+		        return apellidos;
+		    }
+
+		    public void setApellidos(String apellidos) {
+		        this.apellidos = apellidos;
+		    }
 	}
 
-}
+
--- src/paqueteBiblioteca/Ejemplares.java
+++ src/paqueteBiblioteca/Ejemplares.java
@@ -1,5 +1,37 @@
 package paqueteBiblioteca;
 
-public class Ejemplares {
+class Ejemplar {
+    private int id;
+    private String estadoFisico;
+    private String tituloLibro;
 
+    public Ejemplar(int id, String estadoFisico, String tituloLibro) {
+        this.id = id;
+        this.estadoFisico = estadoFisico;
+        this.tituloLibro = tituloLibro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEstadoFisico() {
+        return estadoFisico;
+    }
+
+    public void setEstadoFisico(String estadoFisico) {
+        this.estadoFisico = estadoFisico;
+    }
+
+    public String getTituloLibro() {
+        return tituloLibro;
+    }
+
+    public void setTituloLibro(String tituloLibro) {
+        this.tituloLibro = tituloLibro;
+    }
 }
